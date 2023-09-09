06b99bda6863b11ca30d53be27239ff3b372966a
David Wamai <96637893+davwamai@users.noreply.github.com>
2023-09-09 01:01:10 -0400

    data collection
    data

--- DAQ_design/v0.2.1/revB/monitor.py
+++ DAQ_design/v0.2.1/revB/monitor.py
@@ -1,9 +1,9 @@
 import serial
 import matplotlib.pyplot as plt
-from time import sleep
+import matplotlib
+matplotlib.use('TkAgg')
 
 # Set up the serial connection.
-# Replace 'COM3' with your Arduino's COM port and adjust the baud rate if necessary.
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 ser.flush()
 
@@ -13,48 +13,41 @@ drop_voltages = []
 sense_voltages = []
 resistances = []
 
+num_points = 60  # Number of data points to collect
+
 try:
-    while True:
+    for _ in range(num_points):
         # Read a line from the serial port.
         line = ser.readline().decode('utf-8').strip()
         data = line.split(',')
 
         if len(data) == 5:  # Ensure we have 5 data points before processing.
-            # Append data to lists.
+            # Append data to thier own lists.
             times.append(len(times))
             drop_voltages.append(float(data[1]))
             sense_voltages.append(float(data[3]))
             resistances.append(float(data[4]))
 
-            # Clear the previous plots.
-            plt.clf()
+    # Plot drop and sense 
+    plt.subplot(2, 1, 1)
+    plt.plot(times, drop_voltages, color='blue', label='Drop Voltage')
+    plt.plot(times, sense_voltages, color='red', label='Sense Voltage')
+    plt.legend(loc='upper right')
+    plt.title('Drop and Sense Voltages over Time')
+    plt.ylabel('Voltage (V)')
 
-            # Plot drop voltages.
-            plt.subplot(3, 1, 1)
-            plt.plot(times, drop_voltages, label='Drop Voltage')
-            plt.legend(loc='upper right')
-            plt.title('Drop Voltage over Time')
-            plt.ylabel('Voltage (V)')
+    # Plot resistance
+    plt.subplot(2, 1, 2)
+    plt.plot(times, resistances, color='green', label='Structure Resistance')
+    plt.legend(loc='upper right')
+    plt.title('Structure Resistance over Time')
+    plt.ylabel('Resistance (Ohms)')
+    plt.xlabel('Time (s)')
 
-            # Plot sense voltages.
-            plt.subplot(3, 1, 2)
-            plt.plot(times, sense_voltages, label='Sense Voltage')
-            plt.legend(loc='upper right')
-            plt.title('Sense Voltage over Time')
-            plt.ylabel('Voltage (V)')
-
-            # Plot resistances.
-            plt.subplot(3, 1, 3)
-            plt.plot(times, resistances, label='Structure Resistance')
-            plt.legend(loc='upper right')
-            plt.title('Structure Resistance over Time')
-            plt.ylabel('Resistance (Ohms)')
-            plt.xlabel('Time (s)')
-
-            # Update the plots.
-            plt.pause(0.05)
-
-except KeyboardInterrupt:
-    # Close the serial connection upon exiting.
-    ser.close()
+    plt.tight_layout()  
     plt.show()
+
+finally:
+    ser.close()
+
+
